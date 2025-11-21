b536ad63215530e8142a7ab5dc65f1d9ccf52fa8
Nirush Man Shrestha <nirushstha@gmail.com>
2025-11-21 17:57:02 +0545

    Update game.routes.js

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,15 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const gameController = require("../controllers/game.controller");
-const authMiddleware = require("../middleware/auth.middleware");
 
-// All routes require authentication
-router.use(authMiddleware);
-
-router.post("/init", gameController.initGame);
-router.post("/update-score", gameController.updateScore);
-router.post("/daily-reward", gameController.dailyReward);
-router.get("/leaderboard", gameController.getLeaderboard);
-router.post("/buy-powerup", gameController.buyPowerup);
-
-module.exports = router;
