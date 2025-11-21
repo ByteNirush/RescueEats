0c0148b3c7aa814965f68889edeb64f08de554d3
Nirush Man Shrestha <nirushstha@gmail.com>
2025-11-21 18:00:09 +0545

    Add game routes with authentication middleware

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,1 +1,20 @@
+import express from "express";
+import {
+  initGame,
+  updateScore,
+  dailyReward,
+  getLeaderboard,
+  buyPowerup,
+} from "../controllers/game.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
+const router = express.Router();
+
+// All routes require authentication
+router.post("/init", verifyToken, initGame);
+router.post("/update-score", verifyToken, updateScore);
+router.post("/daily-reward", verifyToken, dailyReward);
+router.get("/leaderboard", verifyToken, getLeaderboard);
+router.post("/buy-powerup", verifyToken, buyPowerup);
+
+export default router;
